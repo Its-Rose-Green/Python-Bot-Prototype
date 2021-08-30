9f90a71b6c3920cd3a8d2a5fa2fc141291144b8a
Rose Green <throwawaytrain20@gmail.com>
2021-08-30 16:11:24 -0600

    Travelling implemented, started on Economy and Tavern. Still need to work on separating this chonky boy into different files. Look into Cogs.

--- protobotmain.py
+++ protobotmain.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 import discord
 from discord.ext import commands
 
@@ -28,6 +28,84 @@ async def on_member_join(member):
 # When they use command prefix with name, like >ping
 @bot.command()
 async def ping(ctx):
-    await ctx.send('message')
+    await ctx.send('pong')
+
+'''TRAVELLING'''
+
+# Reacting to a preset destination in the travel-destinations text channel will DM the reactor and invite
+# to whatever destination server they reacted to.
+
+# TODO: kick the user out of the original server after sending the invite
+@bot.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+
+    # compares the channel id of the message being reacted to, to every channel in that guild, making sure it only executes when
+    # the name of the text channel is travel-destination
+    if (payload.channel_id == discord.utils.get(bot.get_guild(payload.guild_id).channels, name='travel-destinations').id):
+        msg = await bot.get_channel(payload.channel_id).fetch_message(message_id)
+        if (msg.content == 'Windvale'):
+            # A link to the Windvale discord will be sent
+            await payload.member.send('https://discord.gg/jJ3FetRgVu')
+        
+        await msg.remove_reaction(payload.emoji, payload.member)
+
+
+'''TAVERN THINGS'''
+@bot.command()
+async def purchase(ctx, amount: int, item):
+    if (ctx.channel.id != discord.utils.get(ctx.guild.channels, name='tavern').id):
+        return
+    else:
+        await ctx.send("You have purchased " + str(amount) + " " + item)
     
+'''ECONOMY THINGS'''
+@bot.command()
+async def balance(ctx):
+    await open_account(ctx.author)
+
+    users = await get_bank_data()
+
+    pouch_amt = users[str(ctx.author.id)]["pouch"]
+    bank_amt = users[str(ctx.author.id)]["bank"]
+
+    em = discord.Embed(title = f"{ctx.author.name}'s balance")
+    em.add_field(name = "Pouch Amount", value = pouch_amt)
+    em.add_field(name = "Bank Balance", value = bank_amt)
+    await ctx.send(embed = em)
+
+@bot.command()
+@commands.has_role('Creator')
+async def give_myself_money(ctx):
+    await open_account(ctx.author)
+    users = await get_bank_data()
+
+    earnings = 1000
+    await ctx.send(f"Obtained {earnings} coins")
+
+    users[str(ctx.author.id)]["pouch"] += earnings
+
+    with open('centralbank.json', 'w') as file:
+        json.dump(users,file)
+
+async def open_account(user):
+    users = await get_bank_data()
+
+    if str(user.id) in users:
+        return False
+    else:
+        users[str(user.id)] = {}
+        users[str(user.id)]["pouch"] = 0
+        users[str(user.id)]["bank"] = 0
+
+    with open('centralbank.json', 'w') as file:
+        json.dump(users, file)
+    
+    return True
+
+async def get_bank_data():
+    with open('centralbank.json', 'r') as file:
+        users = json.load(file)
+    return users
+
 bot.run(TOKEN)

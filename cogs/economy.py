d51ba9d57964d5cb0713a27b46b1a4a46ef31a90
Rose Green <throwawaytrain20@gmail.com>
2021-09-21 13:56:43 -0600

    More economy things

--- cogs/economy.py
+++ cogs/economy.py
@@ -53,6 +53,62 @@ class Economy(commands.Cog):
         await context.send(f"Lost {amount} coins")
         await self.subtract_money(context.author, amount)
 
+    @commands.command(name="withdraw")
+    async def withdraw(self, context, amount = None):
+        """ Transfers money from bank to pouch
+
+        Input:  commands.Context, amount(Str)
+        Output: None
+        """
+        await self.open_acount(context.author)
+
+        if ((amount == None) | (amount <= 0)):
+            await context.send("Please enter a positive and valid amount.")
+            return
+
+        amount = int(amount)
+    
+        bal = await self.update_bank(context.author)
+        
+        
+        if (amount > bal[1]):
+            await context.send("Too poor.")
+            return
+        
+        await self.update_bank(context.author, amount, "pouch")
+        await self.update_bank(context.author, (-1)*amount, "bank")
+
+        await context.send(f"You withdrew {amount} from your bank.")
+        return
+
+    @commands.command(name="deposit")
+    async def deposit(self, context, amount = None):
+        """ Transfers money from pouch to bank
+
+        Input:  commands.Context, amount(Str)
+        Output: None
+        """
+        await self.open_acount(context.author)
+
+        if ((amount == None) | (amount <= 0)):
+            await context.send("Please enter a positive and valid amount.")
+            return
+
+        amount = int(amount)
+    
+        bal = await self.update_bank(context.author)
+        
+        
+        if (amount > bal[0]):
+            await context.send("Don't have that much in your pouch.")
+            return
+        
+        await self.update_bank(context.author, amount, "bank")
+        await self.update_bank(context.author, (-1)*amount, "pouch")
+
+        await context.send(f"You deposited {amount} to your bank.")
+        return
+
     async def add_money(self, user, amount):
         """ Helper function, adds money to user's pouch
 
@@ -120,6 +176,22 @@ class Economy(commands.Cog):
         with open('centralbank.json', 'r') as file:
             users = json.load(file)
         return users
+
+    async def update_bank(self, user, change = 0, mode = "pouch"):
+        """ Helper function, gets a specific user's pouch value
+
+        Input:  discord.User, change(Int), mode(Str)
+        Output: (Dict) Updated values of user pouch and bank
+        """
+        users = self.get_bank_data()
+
+        users[str(user.id)][mode] += change
+
+        with open('centralbank.json', 'w') as file:
+            json.dump(users, file)
+
+        bal = [users[str(user.id)]["pouch"], users[str(user.id)]["bank"]]
+        return user
 
     async def get_user_pouch_value(self, user):
         """ Helper function, gets a specific user's pouch value
